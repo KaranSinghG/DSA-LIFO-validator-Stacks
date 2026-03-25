4278b7dc9d07c9ce04250b6c4bde92cf67952051
KaranSinghG <karansinghg@ymail.com>
2026-03-25 21:50:10 +0530

    implemented isBalanced logic

--- validator/SyntaxValidator.java
+++ validator/SyntaxValidator.java
@@ -1,9 +1,31 @@
 package validator;
 
+import implementation.ArrayStack;
+
 public class SyntaxValidator {
-    
+    ArrayStack<Character> stack = new ArrayStack<Character>();
+
     public boolean isBalanced(String expression) {
-        // Implementation of balanced parentheses validation
-        return true; // Placeholder return value
+        for(char ch : expression.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else if (ch == ')' || ch == '}' || ch == ']') {
+                try {
+                    char top = stack.pop();
+                    if (!isMatch(ch, top)) {
+                        return false;
+                    }
+                } catch (Exception e) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    private boolean isMatch(char ch, char top) {
+        return (ch == ')' && top == '(') ||
+            (ch == '}' && top == '{') ||
+            (ch == ']' && top == '[');
     }
 }
